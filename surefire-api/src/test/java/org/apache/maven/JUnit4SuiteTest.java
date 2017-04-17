dc092b4153c77e79d09ee7e678587cd565ed92b0
Tibor17 <tibor17@lycos.com>
2017-04-17 13:54:40 +0200

    [SUREFIRE-1364] Report XML should contain system properties of forked JVM

--- surefire-api/src/test/java/org/apache/maven/JUnit4SuiteTest.java
+++ surefire-api/src/test/java/org/apache/maven/JUnit4SuiteTest.java
@@ -39,6 +39,7 @@ import org.apache.maven.surefire.util.TestsToRunTest;
 import org.apache.maven.surefire.util.UrlUtilsTest;
 import org.apache.maven.surefire.util.internal.ByteBufferTest;
 import org.apache.maven.surefire.util.internal.ConcurrencyUtilsTest;
+import org.apache.maven.surefire.util.internal.ImmutableMapTest;
 import org.apache.maven.surefire.util.internal.StringUtilsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -68,7 +69,8 @@ import org.junit.runners.Suite;
     TestsToRunTest.class,
     UrlUtilsTest.class,
     SpecificTestClassFilterTest.class,
-    FundamentalFilterTest.class
+    FundamentalFilterTest.class,
+    ImmutableMapTest.class
 } )
 @RunWith( Suite.class )
 public class JUnit4SuiteTest
